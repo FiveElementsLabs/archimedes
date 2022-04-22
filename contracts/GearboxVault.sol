b25669c9fe1cda2a483152bbe94e56707cd7b3eb
Nicolas <nicolas@racchi.com>
2022-04-22 20:31:59 +0200

    chore: added license

--- contracts/GearboxVault.sol
+++ contracts/GearboxVault.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import "./tokens/ERC20.sol";
 import "./tokens/ERC4626.sol";
 
 contract GearboxVault is ERC4626 {
-    address immutable creditAccount;
+    address creditAccount;
 
     constructor(
         ERC20 _asset,
@@ -13,6 +12,7 @@ contract GearboxVault is ERC4626 {
         string memory _symbol
     ) ERC4626(_asset, _name, _symbol) {
         asset = _asset;
+        ///@dev create creditAccount from factory
     }
 
     ///@notice return the total amounts of assets
@@ -21,10 +21,13 @@ contract GearboxVault is ERC4626 {
         return 1e18; //max amount of underlying asset managed by smart contract
     }
 
+    ///@dev do something before withdrawing
     function beforeWithdraw(uint256 assets, uint256 shares) internal override {
         require(true);
     }
 
+    ///@dev do something after withdrawing
+    ///@params assets amount of assets
     function afterDeposit(uint256 assets, uint256 shares) internal override {
         require(true);
     }
